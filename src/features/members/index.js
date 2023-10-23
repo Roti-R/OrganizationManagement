aed58e32732bd0cb1ee6cd01ffb267d30bc42665
Thanh Tuanas <godiego0408@gmail.com>
2023-10-23 10:55:33 +0700

    Update index.js

--- src/features/members/index.js
+++ src/features/members/index.js
@@ -1,7 +1,35 @@
+import moment from "moment"
+import { useEffect } from "react"
+import { useDispatch, useSelector } from "react-redux"
+import TitleCard from "../../components/Cards/TitleCard"
+import { openModal } from "../common/modalSlice"
+import { CONFIRMATION_MODAL_CLOSE_TYPES, MODAL_BODY_TYPES } from '../../utils/globalConstantUtil'
+import TrashIcon from '@heroicons/react/24/outline/TrashIcon'
+import { showNotification } from '../common/headerSlice'
+
+const TopSideButtons = () => {
+
+    const dispatch = useDispatch()
+
+    const openAddNewLeadModal = () => {
+        
+    }
+
+    return (
+        <div className="inline-block float-right">
+            <button className="btn px-6 btn-sm normal-case btn-primary" onClick={() => openAddNewLeadModal()}>Add New</button>
+        </div>
+    )
+}
+
+
+
 const Members = () => {
     return(
         <>
-        <h1>This is fukin member</h1>
+            <TitleCard title="Thành viên" topMargin="mt-2" TopSideButtons={<TopSideButtons />}>
+
+            </TitleCard>
         </>
     )
 }
